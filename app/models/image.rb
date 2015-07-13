1319b0322a204d074779db5bcc3077c6735489e8
MLee21 <minnie.lee89@gmail.com>
2015-07-12 22:25:35 -0600

    take out paperclip configs.

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,12 +1,12 @@
 class Image < ActiveRecord::Base
   has_many :events
 
-  has_attached_file :img,
-    styles: { event_manage_list: "75x75>",
-              event_banner: "1064x476#",
-              menu_list: "150x150>",
-              item_list: "400x400>" }
-  validates_attachment_content_type :img, content_type: /\Aimage\/.*\Z/
+  # has_attached_file :img,
+  #   styles: { event_manage_list: "75x75>",
+  #             event_banner: "1064x476#",
+  #             menu_list: "150x150>",
+  #             item_list: "400x400>" }
+  # validates_attachment_content_type :img, content_type: /\Aimage\/.*\Z/
 
   validates :title, presence: true, uniqueness: true
   validates :description, presence: true
